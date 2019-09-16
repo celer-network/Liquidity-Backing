986992214aaff7979573b74158df4dd70bdb65c9
Xuefeng Zhu <frankzhu94@gmail.com>
2019-09-16 15:38:12 -0700

    BE-296: Change erc20 decimal to 18 (#45)

--- contracts/helper/ERC20ExampleToken.sol
+++ contracts/helper/ERC20ExampleToken.sol
@@ -5,7 +5,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 contract ERC20ExampleToken is ERC20 {
     string public name = "ERC20ExampleToken";
     string public symbol = "EET20";
-    uint8 public decimals = 2;
+    uint8 public decimals = 18;
     uint public INITIAL_SUPPLY = 300000 ether;
 
     constructor() public {
